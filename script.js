d5ca7de5f63c421c6c6736023aa9ba338d38c5af
Shyamal <sgprajapati24@gmail.com>
2021-01-03 12:03:46 +0530

    getting last word from txt

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-var txtarea=document.getElementById("txtarea");
-txtarea.onkeyup=function () {
-    
+var txtArea=document.getElementById("txtArea");
+txtArea.onkeyup=function () {
+    var word=getLastWord(txtArea.value);
+    alert(word);
+}
+
+function getLastWord(text) {
+    var result= "";
+    for(var i=text.length;i>0;i--)
+    {
+        if(text.substring(i,i-1) !=" ")
+        {
+            result=text.substring(i,i-1)+result;
+        }
+        else
+        {
+            break;
+        }
+    }    
+    return result;
 }
